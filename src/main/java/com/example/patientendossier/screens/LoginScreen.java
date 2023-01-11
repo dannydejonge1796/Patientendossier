60b9842181939beae8c72e4bf0ccdcab6e117afa
Danny de Jonge <dannydejonge1796@gmail.com>
2023-01-11 22:18:21 +0100

    duplicate form field code in method

--- src/main/java/com/example/patientendossier/screens/LoginScreen.java
+++ src/main/java/com/example/patientendossier/screens/LoginScreen.java
@@ -16,7 +16,7 @@ import javafx.stage.Stage;
 
 public class LoginScreen {
 
-  private Stage stage;
+  private final Stage stage;
   private Scene patientLoginScene;
   private Scene carerLoginScene;
 
@@ -36,20 +36,7 @@ public class LoginScreen {
     grid.setPadding(new Insets(25, 25, 25, 25));
 
     Text txtPatientLogin = new Text("Inloggen voor patiënten");
-    txtPatientLogin.setFont(Font.font(32));
-    grid.add(txtPatientLogin, 0, 0);
-
-    Label lblEmail = new Label("Email:");
-    grid.add(lblEmail, 0, 1);
-
-    TextField tfEmail = new TextField();
-    grid.add(tfEmail, 0, 2);
-
-    Label lblPassword = new Label("Wachtwoord:");
-    grid.add(lblPassword, 0, 3);
-
-    TextField tfPassword = new TextField();
-    grid.add(tfPassword, 0, 4);
+    addFormFields(grid, txtPatientLogin);
 
     Hyperlink hlCarerLogin = new Hyperlink("Bent u zorgverlener? Login hier.");
     grid.add(hlCarerLogin, 0, 5);
@@ -82,6 +69,26 @@ public class LoginScreen {
     grid.setPadding(new Insets(25, 25, 25, 25));
 
     Text txtCarerLogin = new Text("Inloggen voor zorgverleners");
+    addFormFields(grid, txtCarerLogin);
+
+    Hyperlink hlPatientLogin = new Hyperlink("Bent u patiënt? Login hier.");
+    grid.add(hlPatientLogin, 0, 5);
+    GridPane.setHalignment(hlPatientLogin, HPos.RIGHT);
+
+    hlPatientLogin.setOnAction(e -> this.stage.setScene(this.patientLoginScene));
+
+    Button btnLogin = new Button("Inloggen");
+    grid.add(btnLogin, 0, 6);
+
+    BorderPane borderPane = new BorderPane();
+    borderPane.setCenter(grid);
+
+    this.carerLoginScene = new Scene(borderPane);
+    this.stage.setScene(this.carerLoginScene);
+  }
+
+  private void addFormFields(GridPane grid, Text txtCarerLogin)
+  {
     txtCarerLogin.setFont(Font.font(32));
     grid.add(txtCarerLogin, 0, 0);
 
@@ -96,22 +103,5 @@ public class LoginScreen {
 
     TextField tfPassword = new TextField();
     grid.add(tfPassword, 0, 4);
-
-    Hyperlink hlPatientLogin = new Hyperlink("Bent u patiënt? Login hier.");
-    grid.add(hlPatientLogin, 0, 5);
-    GridPane.setHalignment(hlPatientLogin, HPos.RIGHT);
-
-    hlPatientLogin.setOnAction(e -> {
-      this.stage.setScene(this.patientLoginScene);
-    });
-
-    Button btnLogin = new Button("Inloggen");
-    grid.add(btnLogin, 0, 6);
-
-    BorderPane borderPane = new BorderPane();
-    borderPane.setCenter(grid);
-
-    this.carerLoginScene = new Scene(borderPane);
-    this.stage.setScene(this.carerLoginScene);
   }
 }
