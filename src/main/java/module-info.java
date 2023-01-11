02ebd181917d08dc6cfeb046a58d56095704b2e9
Danny de Jonge <dannydejonge1796@gmail.com>
2023-01-11 20:52:14 +0100

    added javafx project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.patientendossier {
+  requires javafx.controls;
+  requires javafx.fxml;
+
+
+  opens com.example.patientendossier to javafx.fxml;
+  exports com.example.patientendossier;
+}
