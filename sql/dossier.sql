9e012b4bffefa8f6a88898c866313c6cc006b9f4
Danny de Jonge <dannydejonge1796@gmail.com>
2023-04-01 14:41:50 +0200

    verandering in db

--- sql/dossier.sql
+++ sql/dossier.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 27 mrt 2023 om 14:06
+-- Gegenereerd op: 01 apr 2023 om 14:41
 -- Serverversie: 10.4.27-MariaDB
--- PHP-versie: 8.1.12
+-- PHP-versie: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,26 +28,24 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `allergy` (
-  `id` int(10) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `description` text NOT NULL
+  `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `allergy`
 --
 
-INSERT INTO `allergy` (`id`, `name`, `description`) VALUES
-(0, 'Hooikoorts', 'pollen van grassen, bomen en planten'),
-(1, 'Huisstofmijt', 'mijten die in huisstof leven'),
-(2, 'Dierenallergie', 'honden, katten, vogels en andere dieren'),
-(3, 'Voedselallergie', 'bijvoorbeeld pinda\'s, schaaldieren of gluten'),
-(4, 'Medicijnallergie', 'zoals penicilline of aspirine'),
-(5, 'Insectensteken', 'bijen, wespen, muggen, etc.'),
-(6, 'Schimmelallergie', 'schimmels die binnenshuis groeien'),
-(7, 'Nikkelallergie', 'in sieraden, munten, etc.'),
-(8, 'Cosmetica-allergie', 'huidverzorgingsproducten, parfums, etc.'),
-(9, 'Latexallergie', 'latex handschoenen, ballonnen, etc.');
+INSERT INTO `allergy` (`name`) VALUES
+('Cosmetica-allergie'),
+('Dierenallergie'),
+('Hooikoorts'),
+('Huisstofmijt'),
+('Insectensteken'),
+('Latexallergie'),
+('Medicijnallergie'),
+('Nikkelallergie'),
+('Schimmelallergie'),
+('Voedselallergie');
 
 -- --------------------------------------------------------
 
@@ -56,16 +54,20 @@ INSERT INTO `allergy` (`id`, `name`, `description`) VALUES
 --
 
 CREATE TABLE `allergy_patient` (
-  `allergy_id` int(10) NOT NULL,
-  `patient_number` int(10) NOT NULL
+  `allergy_name` varchar(255) NOT NULL,
+  `patient_number` int(10) NOT NULL,
+  `description` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `allergy_patient`
 --
 
-INSERT INTO `allergy_patient` (`allergy_id`, `patient_number`) VALUES
-(0, 1234567890);
+INSERT INTO `allergy_patient` (`allergy_name`, `patient_number`, `description`) VALUES
+('Cosmetica-allergie', 1234567890, 'blablabla'),
+('Hooikoorts', 1234567890, 'pollen van'),
+('Huisstofmijt', 1234567890, 'sfdgsdfgsdfg'),
+('Latexallergie', 1234567890, 'lkgfhlksdhglhfdsghlsdflgsdfklgsdflk');
 
 -- --------------------------------------------------------
 
@@ -115,9 +117,19 @@ INSERT INTO `care_patient` (`care_number`, `patient_number`) VALUES
 --
 
 CREATE TABLE `health` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `description` text NOT NULL
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `health_patient`
+--
+
+CREATE TABLE `health_patient` (
+  `health_name` varchar(255) NOT NULL,
+  `patient_number` int(10) NOT NULL,
+  `description` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -127,27 +139,24 @@ CREATE TABLE `health` (
 --
 
 CREATE TABLE `medicine` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `dosage` varchar(10) NOT NULL
+  `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `medicine`
 --
 
-INSERT INTO `medicine` (`id`, `name`, `description`, `dosage`) VALUES
-(0, 'Paracetamol', 'Een pijnstiller die vaak wordt gebruikt voor milde tot matige pijn', '500 mg'),
-(1, 'Ibuprofen', 'Een ontstekingsremmende pijnstiller die vaak wordt gebruikt bij artritis en menstruatiepijn', '200 mg'),
-(2, 'Aspirine', 'Een pijnstiller en koortsverlager die ook bloedverdunnende eigenschappen heeft', '500 mg'),
-(3, 'Diazepam', 'Een kalmeringsmiddel dat wordt gebruikt om angst, slapeloosheid en spierkrampen te behandelen', '2-10 mg'),
-(4, 'Lorazepam', 'Een kalmeringsmiddel dat wordt gebruikt om angst en slapeloosheid te behandelen', '1-4 mg'),
-(5, 'Omeprazol', 'Een zuurremmer die wordt gebruikt voor de behandeling van maagzweren, reflux en indigestie', '20-40 mg'),
-(6, 'Sertraline', 'Een antidepressivum dat wordt gebruikt voor de behandeling van depressie, angst en paniekstoornissen', '200 mg'),
-(7, 'Metformine', 'Een medicijn voor diabetes type 2 dat de bloedsuikerspiegel verlaagt', '1000 mg'),
-(8, 'Salbutamol', 'Een luchtwegverwijder die wordt gebruikt voor de behandeling van astma en chronische obstructieve longziekte (COPD)', '200 mic'),
-(9, 'Furosemide', 'Een plasmiddel dat wordt gebruikt voor de behandeling van oedeem en hoge bloeddruk', '80 mg');
+INSERT INTO `medicine` (`name`) VALUES
+('Aspirine'),
+('Diazepam'),
+('Furosemide'),
+('Ibuprofen'),
+('Lorazepam'),
+('Metformine'),
+('Omeprazol'),
+('Paracetamol'),
+('Salbutamol'),
+('Sertraline');
 
 -- --------------------------------------------------------
 
@@ -157,7 +166,9 @@ INSERT INTO `medicine` (`id`, `name`, `description`, `dosage`) VALUES
 
 CREATE TABLE `medicine_patient` (
   `patient_number` int(10) NOT NULL,
-  `medicine_id` int(10) NOT NULL
+  `medicine_name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `dosage` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -191,13 +202,13 @@ INSERT INTO `patient` (`number`, `firstname`, `lastname`, `birthdate`, `phonenum
 -- Indexen voor tabel `allergy`
 --
 ALTER TABLE `allergy`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`name`);
 
 --
 -- Indexen voor tabel `allergy_patient`
 --
 ALTER TABLE `allergy_patient`
-  ADD PRIMARY KEY (`allergy_id`,`patient_number`);
+  ADD PRIMARY KEY (`allergy_name`,`patient_number`);
 
 --
 -- Indexen voor tabel `care`
@@ -212,16 +223,28 @@ ALTER TABLE `care_patient`
   ADD PRIMARY KEY (`care_number`,`patient_number`);
 
 --
+-- Indexen voor tabel `health`
+--
+ALTER TABLE `health`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Indexen voor tabel `health_patient`
+--
+ALTER TABLE `health_patient`
+  ADD PRIMARY KEY (`health_name`,`patient_number`);
+
+--
 -- Indexen voor tabel `medicine`
 --
 ALTER TABLE `medicine`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`name`);
 
 --
 -- Indexen voor tabel `medicine_patient`
 --
 ALTER TABLE `medicine_patient`
-  ADD PRIMARY KEY (`patient_number`,`medicine_id`);
+  ADD PRIMARY KEY (`patient_number`,`medicine_name`);
 
 --
 -- Indexen voor tabel `patient`
--- src/main/java/com/example/patientendossier/model/Allergy.java
+++ src/main/java/com/example/patientendossier/model/Allergy.java
@@ -1,25 +1,13 @@
 package com.example.patientendossier.model;
 
-import com.example.patientendossier.Application;
-
 public class Allergy {
 
-  private String name;
+  private final String name;
   private String description;
 
   public Allergy(String name, String description) {
     this.name = name;
     this.description = description;
-  }
-
-  public void store()
-  {
-
-  }
-
-  public void update()
-  {
-
   }
 
   public String getName() {
@@ -28,11 +16,6 @@ public class Allergy {
 
   public String getDescription() {
     return description;
-  }
-
-  public void setName(String name)
-  {
-    this.name = name;
   }
 
   public void setDescription(String description) {
