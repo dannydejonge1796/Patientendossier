3ad805c8e4bf88237ae64cd64742b4202bf3a2be
Danny de Jonge <dannydejonge1796@gmail.com>
2023-04-09 16:39:46 +0200

    comments voor de informatie pagina en de herbruikbare tabel

--- src/main/java/com/example/patientendossier/screen/InfoPageScreen.java
+++ src/main/java/com/example/patientendossier/screen/InfoPageScreen.java
@@ -26,44 +26,58 @@ public class InfoPageScreen {
 
   private void createInfoPagePane()
   {
+    //Vbox aanmaken
     this.vBoxInfoPage = new VBox();
     vBoxInfoPage.setMaxWidth(1000);
     vBoxInfoPage.setPadding(new Insets(20,20,20,20));
     vBoxInfoPage.setSpacing(15);
 
+    //HBox voor bovenste gedeelte aanmaken
     HBox hBoxTop = new HBox();
     hBoxTop.setPrefWidth(950);
 
+    //HBox aanmaken voor label
     HBox hBoxLblPage = new HBox();
+    //Nieuwe label aanmaken en toevoegen
     this.lblPage = new Label();
     lblPage.setFont(Font.font("Arial", FontWeight.BOLD, 20));
     hBoxLblPage.getChildren().add(lblPage);
+    //HBox in de volledige breedte laten uitstrekken
     HBox.setHgrow(hBoxLblPage, Priority.SOMETIMES);
 
+    //Knop toevoegen aanmaken en op onzichtbaar instellen
     this.btnAdd = new Button("Toevoegen");
     this.btnAdd.setVisible(false);
-
+    //Label en knop toevoegen
     hBoxTop.getChildren().addAll(hBoxLblPage, this.btnAdd);
+    //HBox toevoegen aan de vbox van de pagina
     vBoxInfoPage.getChildren().add(hBoxTop);
 
+    //Een pane aanmaken waar de tabel moet komen en vervolgens toevoegen
     this.hBoxTable = new HBox();
     hBoxTable.setPrefWidth(950);
     vBoxInfoPage.getChildren().add(hBoxTable);
 
+    //Een HBox aanmaken voor het onderste gedeelte van het scherm
     HBox hBoxBottom = new HBox();
 
+    //Knop verwijderen aanmaken, op onzichtbaar zetten en onbeschikbaar maken
     this.btnDelete = new Button("Verwijderen");
     this.btnDelete.setVisible(false);
     this.btnDelete.setDisable(true);
 
+    //Region aanmaken voor ruimte tussen de twee knoppen
     Region regionBottom = new Region();
     HBox.setHgrow(regionBottom, Priority.ALWAYS);
 
+    //Knop wijzigen aanmaken, op onzichtbaar zetten en onbeschikbaar maken
     this.btnUpdate = new Button("Wijzigen");
     this.btnUpdate.setVisible(false);
     this.btnUpdate.setDisable(true);
 
+    //Twee knoppen en region toevoegen aan de HBox van het onderste gedeelte
     hBoxBottom.getChildren().addAll(btnDelete, regionBottom, btnUpdate);
+    //HBox toevoegen aan de vbox van de pagina
     vBoxInfoPage.getChildren().add(hBoxBottom);
   }
 
--- src/main/java/com/example/patientendossier/screen/TableScreen.java
+++ src/main/java/com/example/patientendossier/screen/TableScreen.java
@@ -14,28 +14,42 @@ public class TableScreen {
 
   public <T> TableView<T> createTableView(ArrayList<T> data, String[] columnNames, String[] propertyNames)
   {
+    //Nieuwe tabel view aanmaken
     TableView<T> table = new TableView<>();
+    //Nieuwe observable list aanmaken
     ObservableList<T> olData = FXCollections.observableArrayList();
 
+    //Voeg alles van de arraylist met objecten toe aan de observable lijst
     olData.addAll(data);
+    //Stop de observable lijst in de tabel
     table.setItems(olData);
 
+    //Loop door de kolomnamen heen die zijn meegegeven
     for (int i = 0; i < columnNames.length; i++) {
+      //Maak een nieuwe kolom aan
       TableColumn<T, Object> column = new TableColumn<>(columnNames[i]);
+      //Stop de waardes van de attributen onder de juiste kolom
       column.setCellValueFactory(new PropertyValueFactory<>(propertyNames[i]));
 
-      // Custom cell factory to handle Integer values and allow text wrapping
+      // Definieer een TableCell-fabriek voor een tabelkolom
       column.setCellFactory(tc -> new TableCell<>() {
+
+        // Maak een Text-object aan om de celinhoud weer te geven
         private final Text text = new Text();
 
+        // Override de updateItem methode om de inhoud van de cel bij te werken
         @Override
         protected void updateItem(Object item, boolean empty) {
           super.updateItem(item, empty);
+
+          // Controleer of de cel leeg is
           if (item != null) {
+            // Als de cel niet leeg is, stel de tekst in en pas de wrapping width aan
             text.setText(item.toString());
             text.wrappingWidthProperty().bind(widthProperty());
             setGraphic(text);
           } else {
+            // Als de cel leeg is, verwijder dan de inhoud van de cel
             setGraphic(null);
           }
         }
@@ -44,13 +58,12 @@ public class TableScreen {
       table.getColumns().add(column);
     }
 
+    // Stel het kolomformaat in zodat het past binnen de breedte van de tabel
     table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+    // Voeg een sorteerbare kolom toe aan de tabel en stel deze in als standaard
     table.getSortOrder().add(new TableColumn<>(columnNames[columnNames.length - 1]));
 
     return table;
   }
-
-
-
-
 }
