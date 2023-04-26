a6e238aa61d6bb5443fcc70c4814b0ad562fa604
Danny de Jonge <dannydejonge1796@gmail.com>
2023-04-26 09:16:31 +0200

    styling voor dossier forms

--- src/main/java/com/example/patientendossier/screen/DossierFormScreen.java
+++ src/main/java/com/example/patientendossier/screen/DossierFormScreen.java
@@ -2,6 +2,7 @@ package com.example.patientendossier.screen;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
@@ -22,7 +23,7 @@ public class DossierFormScreen {
     //Nieuwe VBox aanmaken
     this.vBoxFormPage = new VBox();
     vBoxFormPage.setMaxWidth(1260);
-    vBoxFormPage.setPadding(new Insets(20,20,20,20));
+    vBoxFormPage.setPadding(new Insets(25,25,25,25));
     vBoxFormPage.setSpacing(15);
 
     //Nieuw label aanmaken en toevoegen
@@ -37,6 +38,13 @@ public class DossierFormScreen {
     this.gridForm = new GridPane();
     gridForm.setHgap(15);
     gridForm.setVgap(15);
+
+    ColumnConstraints col1 = new ColumnConstraints();
+    col1.setPercentWidth(20); // Set column 1 width to 20%
+    ColumnConstraints col2 = new ColumnConstraints();
+    col2.setPercentWidth(80); // Set column 2 width to 80%
+
+    gridForm.getColumnConstraints().addAll(col1, col2);
 
     //Grid aan form pane toevoegen
     formPane.getChildren().add(gridForm);
